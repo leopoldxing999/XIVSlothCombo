17f0b7999d8e051b828ecef988ce2d2751481c24
x <x@x.com>
2022-09-08 07:31:17 +0800

    3.0.16.21

--- XIVSlothCombo/Core/PluginConfiguration.cs
+++ XIVSlothCombo/Core/PluginConfiguration.cs
@@ -281,7 +281,7 @@ namespace XIVSlothCombo.Core
         /// <summary> Hides the message of the day. </summary>
         public bool HideMessageOfTheDay { get; set; } = false;
         
-        public bool SetOutChat { get; set; } = false;
+        public bool SetOutChat { get; set; } = true;
 
         /// <summary> Save the configuration to disk. </summary>
         public void Save() => Service.Interface.SavePluginConfig(this);
